bd0edc8b9d9f781a675e14aa72ade963685b0384
v.vardanyan <vako3008@gmail.com>
2025-11-13 16:14:02 +0400

    fix

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -6,7 +6,7 @@ import gsap from 'gsap';
 const Target = (props) => {
   const targetRef = useRef();
   const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
+    'https://files.renderdraw.us/renderings/public/gltf/clubcar/dtest3/XRT_850draco-processed.gltf',
   );
 
   useGSAP(() => {
