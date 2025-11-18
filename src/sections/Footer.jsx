67fec965c464da080eb90667c70311f1a4d41d3c
v.vardanyan <vako3008@gmail.com>
2025-11-19 00:09:07 +0400

    improvements

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,15 +2,15 @@ const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
       <div className="flex gap-3">
-        <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        <a className="social-icon" href="https://www.linkedin.com/in/vardges-vardanyan/" target="_blank" rel="noreferrer">
+          <img src="/assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2" />
+        </a>
+        <a className="social-icon" href="https://wakcination.t.me" target="_blank" rel="noreferrer">
+          <img src="/assets/telegram.svg" alt="telegram" className="w-1/2 h-1/2" />
+        </a>
+        <a className="social-icon" href="https://www.instagram.com/vakooo" target="_blank" rel="noreferrer">
           <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
       </div>
 
       <p className="text-white-500">© {new Date().getFullYear()} Vardges Vardanyan. All rights reserved.</p>
