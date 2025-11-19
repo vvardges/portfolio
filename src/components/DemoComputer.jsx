8f404ef7c172afe3bc3f9f75216cbe63669583dc
v.vardanyan <vako3008@gmail.com>
2025-11-19 13:29:41 +0400

    fix mobile

--- src/components/DemoComputer.jsx
+++ src/components/DemoComputer.jsx
@@ -1,11 +1,11 @@
 import { useRef, useEffect } from 'react';
-import { useGLTF, useAnimations, useVideoTexture } from '@react-three/drei';
+import { useGLTF, useVideoTexture } from '@react-three/drei';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
 const DemoComputer = (props) => {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF('/models/computer.glb');
+  const { nodes, materials } = useGLTF('/models/computer.glb');
 
   const txt = useVideoTexture(props.texture ? props.texture : '/textures/project/project1.mp4', {
     playsInline: true,
